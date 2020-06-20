ae5ab153212d1216866b07ec6375c1c58cd15fcc
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-06-20 13:32:11 -0500

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:listdemo_app/screens/home_screen.dart';
+ 
+
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: HomeScreen(),
+      
+        );
+  }
+}
